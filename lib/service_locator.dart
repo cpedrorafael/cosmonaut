39dfb8fd13c5074a976c3a395697d74207278045
Pedro Cutino <pedro.cutino23@gmail.com>
2021-05-22 13:26:32 -0500

    service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,7 +1,48 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+import 'core/network/network_info.dart';
+import 'features/article/data/datasources/article_local_data_source.dart';
+import 'features/article/data/datasources/article_remote_data_source.dart';
+import 'features/article/data/repositories/article_repository_impl.dart';
+import 'features/article/domain/repositories/article_repository.dart';
+import 'features/article/domain/usecases/get_articles.dart';
+import 'features/article/presentation/bloc/article_bloc.dart';
 
 final locator = GetIt.instance;
 
 Future setupServiceLocator() async {
   locator.reset();
+
+  //Article bloc
+  locator.registerFactory(() => ArticleBloc(getArticles: locator()));
+
+  //Use cases
+  locator.registerLazySingleton(() => GetArticles(locator()));
+
+  //Repository
+  locator.registerLazySingleton<ArticleRepository>(() => ArticleRepositoryImpl(
+        localDataSource: locator(),
+        remoteDataSource: locator(),
+        networkInfo: locator(),
+      ));
+
+  //Data sources
+  locator.registerLazySingleton<ArticleLocalDataSource>(
+      () => ArticleLocalDataSourceImpl(storage: locator()));
+
+  locator.registerLazySingleton<ArticleRemoteDataSource>(
+      () => ArticleRemoteDataSourceImpl(
+            client: locator(),
+          ));
+
+  //Core
+  locator.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(locator()));
+
+  //3rd party libs
+  locator.registerLazySingleton(() => FlutterSecureStorage());
+  locator.registerLazySingleton(() => http.Client);
+  locator.registerLazySingleton(() => DataConnectionChecker());
 }
